6c7664e948ed15f66300391a5d2cb28ed2919087
Bhavesh <choudharybhavesh5@gmail.com>
2022-07-14 18:27:20 +0530

    Initial Commit

---
+++ src/main/java/org/interviews/LeapYear.java
@@ -0,0 +1,18 @@
+package org.interviews;
+
+public class LeapYear {
+    private final int year;
+
+    public LeapYear(int year) {
+        this.year = year;
+    }
+
+    public boolean checkLeapYear() {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,44 @@
+import org.interviews.LeapYear;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LeapYearTest {
+
+    // Divisible by 400 - Leap year  - 2000
+    // Divisible by 100 & !400  - Not Leap year   - 1700, 1800, 1900
+    // Divisible by 4 & !100 - Leap year   - 2008,2012,2016
+    // Divisible by !4 = not leap year  - 2017,2018,2019
+
+
+    @Test
+    public void divisibleBy400(){
+        LeapYear lp = new LeapYear(2000);
+        assertTrue(lp.checkLeapYear());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1700,1800,1900})
+    public void divisibleBy100andNot400(int year){
+        LeapYear lp = new LeapYear(year);
+        assertFalse(lp.checkLeapYear());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2008,2012,2016})
+    public void divisibleBy4andNot100(int year){
+        LeapYear lp = new LeapYear(year);
+        assertTrue(lp.checkLeapYear());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {2017,2018,2019})
+    public void notDivisibleBy4(int year){
+        LeapYear lp = new LeapYear(year);
+        assertFalse(lp.checkLeapYear());
+    }
+
+}
